b70e157fdc11a05cf0290779dc2fb1ec68bd462b
diiegosk <diegoss2502@gmail.com>
2025-10-26 23:44:52 +0100

    payo

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>ForoPayos</title>
+<meta name="description" content="Foro pa payos">
+<link rel="icon" type="image" href="https://www.denia.com/wp-content/uploads/2015/05/logo-payo.jpg">
+</head>
+<body>
+    <h1>
+        Me cago en sos
+    </h1>
+
+    <form>
+    <h2>
+        Que payo eres
+    </h2>
+    <input type="text">
+    
+    <h2>
+        Que paya te zumbas
+    </h2>
+
+    <input type="text">
+
+    <input type="submit">
+
+    </form>
+
+</body>
+</html>
